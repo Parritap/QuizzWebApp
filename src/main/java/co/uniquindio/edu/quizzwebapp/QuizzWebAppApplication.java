d7cb0f4bc9fe192473c325994bb33889c8005eb9
Juan Esteban Parra Parra <juane.parrap@uqvirtual.edu.co>
2023-10-09 21:15:04 -0500

    Initial commit

---
+++ src/main/java/co/uniquindio/edu/quizzwebapp/QuizzWebAppApplication.java
@@ -0,0 +1,13 @@
+package co.uniquindio.edu.quizzwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizzWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/uniquindio/edu/quizzwebapp/QuizzWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uniquindio.edu.quizzwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
